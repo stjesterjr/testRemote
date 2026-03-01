80ebfa3420a1ad7e773c2376264aade3cfd0f8d8
Andrey Markov <st.jesssssco@gmail.com>
2026-03-01 13:21:20 +0300

    add sources to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
+#include <ostream>
+#include <type_traits>
+
+
+
+template<typename T, typename = void>
+struct IsPrintable : std::false_type{};
+
+template<typename T>
+struct IsPrintable<T, std::void_t<decltype(
+    std::declval<std::ostream>() << std::declval<T>()
+)
+>>: std::true_type{};
+
+static_assert(IsPrintable<int>{} == true, "");
+
+
+template<typename... Args, typename = std::enable_if<IsPrintable<Args>{}...>>
+void print(Args&&... args){
+    (std::cout << ... << args);
+}
 
 int main(){
     std::cout << "Hi!";
